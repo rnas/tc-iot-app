1bd08a9714b852c4988240ca11dbc82e625c6100
rnas <rnaswaty@gmail.com>
2017-09-28 20:53:11 -0300

    first commit

---
+++ app/src/main/java/rnas/com/br/porteiro/MainActivity.java
@@ -0,0 +1,68 @@
+package rnas.com.br.porteiro;
+
+import android.graphics.PointF;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+
+
+    private QRCodeReaderView qrCodeReaderView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO : check and ask for permissions
+
+
+        qrCodeReaderView = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+
+        // Use this function to enable/disable decoding
+        qrCodeReaderView.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        qrCodeReaderView.setAutofocusInterval(5000L);
+
+        // Use this function to enable/disable Torch
+        qrCodeReaderView.setTorchEnabled(true);
+
+        // Use this function to set back camera preview
+        qrCodeReaderView.setBackCamera();
+
+    }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+        qrCodeReaderView.stopCamera();
+
+        
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        qrCodeReaderView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
+    }
+}
